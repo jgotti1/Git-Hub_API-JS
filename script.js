8fa229fa05dea3af26551164f222504e7e49e2f2
jgotti1 <jgotti1@optonline.net>
2023-08-08 21:07:38 -0400

    some css changes

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const form = document.getElementById("form-selector");
 const getUserNameInput = document.getElementById("username");
 const displayUsername = document.getElementById("display-username"); // Get the display element
+const displayUsernameText = document.querySelector(".username-text"); // Get the display element
+
 const jsonData = document.getElementById("display-jsonData");
 const avatarImage = document.getElementById("avatar");
 const avatarHead = document.getElementById("avatar-head");
 const errorMessage = document.getElementById("error-message");
+const informationSection = document.querySelector(".information-link-section");
+const informationSectionGitLink = document.querySelector(".information-link-section a");
+const clear = document.getElementById("clear");
+
+clear.addEventListener("click", function () {
+  location.reload(); // Reload the window
+});
 
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
   const userName = getUserNameInput.value;
-  displayUsername.textContent = `User Name Entered: ${userName}`;
+  
+  displayUsernameText.style.display = "block";
+  displayUsername.textContent = ` ${userName}`;
+  
 
   const response = await fetch(`https://api.github.com/users/${userName}`);
 
@@ -23,6 +35,8 @@ form.addEventListener("submit", async function (event) {
   jsonData.textContent = `Login: ${returnedData.login}`;
   avatarImage.src = returnedData.avatar_url;
   avatarHead.textContent = `Profile Photo for "${userName}"`;
+  informationSection.style.display = "block";
+  informationSectionGitLink.href = `https://github.com/${userName}`;
 
   console.log(returnedData);
 });
